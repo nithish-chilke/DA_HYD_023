5ff1562efb61edd9b30ac0f50b2a2fdd86174ed4
nithish-chilke <chilkenithish465@gmail.com>
2026-07-28 13:18:57 +0530

    Tokens-Day1

---
+++ Day-01.py
@@ -0,0 +1,16 @@
+print("Hello Codegan")
+'''
+Tokens --> A smallest unit in a program.
+'''
+name = 'Nithish'
+print(name)
+email_id= 'chilkenithish465@gmail.com'
+print(email_id)
+hospital= ' amr '
+print(hospital)
+
+#multiassignment of variables
+name,location,age = 'codegan','hyd',7
+print(name)
+print(location)
+print(age)
