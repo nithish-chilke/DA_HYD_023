ea6d1170fee29965d3cd18b37f1f455d098d0020
nithish-chilke <chilkenithish465@gmail.com>
2026-08-18 13:05:01 +0530

    Function

---
+++ Day-18.py
@@ -0,0 +1,87 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+========= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ========
+15
+codegnan
+[12, 5, 12, 34]
+enter the values:23,-4
+23 -4
+19
+>>> 
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+codegnan
+>>> 
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+codegnan
+9
+None
+>>> 
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+Traceback (most recent call last):
+  File "C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py", line 47, in <module>
+    print(details())
+  File "C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py", line 46, in details
+    return name,age,salary
+NameError: name 'name' is not defined
+>>> 
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+('saketh', 32, 500000)
+>>> 
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+codegnan
+>>> 
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+None
+>>> 
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+('saketh', 'codegnan')
+('sai', 'vizag')
+Traceback (most recent call last):
+  File "C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py", line 69, in <module>
+    print(details("vizag","shyam",34))
+TypeError: details() takes 2 positional arguments but 3 were given
+>>> 
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+Enter the value:codegana,hudera
+Name is codegana
+Place is hudera
+>>> 
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+The Item ismilK and price is 32
+The Item isBread and price is 35
+The Item isBread and price is 45
+
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+The Item is milk and price is 32
+The Item is Bread and price is 35
+The Item is Bread and price is 45
+
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+The Item is milk and price is 32
+The Item is Bread and price is 100
+The Item is Bread and price is 45
+
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+The Item is milk and price is 32
+The Item is Bread and price is 100
+The Item is Bread and price is 45
+The Item is cheese and price is 100
+
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+The Item is milk and price is 32
+The Item is Bread and price is 100
+The Item is Bread and price is 45
+The Item is cheese and price is 100
+
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+Employee name is sai,role is Admin and salary is 20000,works in placecodegnan
+Traceback (most recent call last):
+  File "C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py", line 96, in <module>
+    employee(salry=25000,role="Frontdesk",name="Asha")
+TypeError: employee() got an unexpected keyword argument 'salry'. Did you mean 'salary'?
+
+============= RESTART: C:/Users/chilk/OneDrive/Desktop/DA_HYD_023/Day-18.py ============
+Employee name is sai,role is Admin and salary is 20000,works in placecodegnan
+Employee name is Asha,role is Frontdesk and salary is 25000,works in placecodegnan
+Employee name is Aksah,role is IT and salary is 250000,works in placecognizant
